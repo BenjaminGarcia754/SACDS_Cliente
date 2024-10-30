d29344841039b02d7b12791f171e2d3b93fc5873
Jesus Tlapa <jesus.tlapa11@gmail.com>
2024-10-30 16:17:13 -0600

    Interfaz login incorporada

--- index.php
+++ index.php
@@ -27,36 +27,66 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión</title>
+    <title>Inicio de Sesión</title>
+    <link rel="stylesheet" href="styles.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <h2 class="text-center mt-5">Iniciar Sesión</h2>
-            <?php echo $message; // Mostrar el mensaje de respuesta aquí ?>
-            <form action="index.php" method="post">
-                <div class="form-group">
-                    <label for="username">Correo</label>
-                    <input type="text" class="form-control" id="username" name="username" required>
+    <div class="header text-center py-4 bg-light rounded shadow">
+        <h1 class="display-4 font-weight-bold text-primary">SISTEMA DE ADMINISTRACIÓN DE DONADORES DE SANGRE</h1>
+        <h2 class="lead text-secondary">CENTRO DE ALTAS ESPECIALIDADES</h2>
+    </div>
+    <div class="bg-white p-4 rounded shadow mx-auto mt-5 w-50" style="max-width: 500px;">
+        <h2 class="text-center">Iniciar Sesión</h2>
+        <?php echo $message;?>
+        <form action="index.php" method="POST">
+            <div class="form-group">
+                <label for="email">Correo</label>
+                <input type="email" class="form-control" id="username" name="username" placeholder="correo@ejemplo.com" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Contraseña</label>
+                <div class="input-group">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="********" required>
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-outline-secondary" onclick="togglePassword('password')">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="password">Contraseña</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Iniciar Sesión</button>
-            </form>
-        </div>
+            </div>
+            <div class="text-center mt-4">
+                <button type="submit" class="btn btn-primary mr-2 w-75">Iniciar</button>
+                <a href="userRegistration.html" class="btn btn-outline-primary w-75 mt-4">Registrarme</a>
+            </div>
+        </form>
     </div>
 </div>
+    <footer class="bg-white py-3 mt-5">
+        <div class="container text-center">
+            <img src="./Images/footerImages.png" class="img-fluid" alt="Imagen 1">
+        </div>
+    </footer>
+    <script>
+        // Función para mostrar/ocultar la contraseña de un campo específico
+        function togglePassword(fieldId) {
+            var passwordInput = document.getElementById(fieldId);
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+            } else {
+                passwordInput.type = "password";
+            }
+        }
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.6/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+              
 </body>
 </html>
-
-
